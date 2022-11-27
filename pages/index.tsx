342f6e74161c845f16d231b82a2c49662f132d5e
Jan Tabara <jan@tabara.cz>
2022-11-27 15:25:20 +0100

    Remove redundant shit

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,6 @@ import Head from "next/head";
 import About from "../components/About";
 import Contact from "../components/Contact";
 import Hero from "../components/Hero";
-import Instagram from "../components/Instagram";
-import Portfolio from "../components/Portfolio";
-import Slider from "../components/Slider";
-import { SliderData } from "../components/SliderData";
 
 export default function Home() {
   return (
@@ -19,13 +15,12 @@ export default function Home() {
         heading="Not just another agency."
         message="We make app development feel easy."
       />
-      <div className="pt-16" style={{ backgroundColor: "#fafafa" }}>
+      <div className="pt-16" style={{ backgroundColor: "#f4f4f4" }}>
         <About />
       </div>
-
-      <Portfolio />
+      {/* <Portfolio />
       <Slider slides={SliderData} />
-      <Instagram />
+      <Instagram /> */}
       <Contact />
     </div>
   );
