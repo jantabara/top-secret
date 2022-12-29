ac6cb4ef4f9e5d823e0301da8543245f51cd700f
Honza Barton <honza.barton@icloud.com>
2022-12-29 10:02:03 +0100

    Fix mobile background

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Hero = ({ heading, message }) => {
   return (
-    <div className="hero flex items-center justify-center h-screen bg-fixed bg-center bg-cover hero__custom-img">
+    <div className="hero flex items-center justify-center h-screen bg-center bg-cover hero__custom-img">
       <div className="hero__overlay absolute top-0 left-0 right-0 bottom-0 bg-black/70 z-[2] flex">
         <div className="p-5 text-white z-[2] m-auto">
           <h2
